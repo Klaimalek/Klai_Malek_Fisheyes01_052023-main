d6ca7e0d4dfdd185572532b80f6342f034c3e222
Malek <absimalekmootez@gmail.com>
2023-06-15 15:28:17 +0200

    améliration de css pour la page photographer

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -130,4 +130,34 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
   validation();
 });
+setFocusOnlyInContainer('.modal','button[type="submit"]')
 
+
+
+//Garder le focus uniquement dans les formulaires,
+function setFocusOnlyInContainer(
+  classContainer,
+  classFirstElement,
+  classLastElement,
+) {
+  document
+    .querySelector(classContainer)
+    .addEventListener('keydown', (event) => {
+      //Si element change de place dans le container
+      let firstElement = document.querySelector(classFirstElement);
+      let lastElement = document.querySelector(classLastElement);
+
+      let isTab = event.key == 'Tab' || event.keyCode == 9;
+      if (event.shiftKey) {
+        if (document.activeElement == firstElement) {
+          firstElement.focus();
+          event.preventDefault();
+        }
+      } else {
+        if (document.activeElement == lastElement) {
+          firstElement.focus();
+          event.preventDefault();
+        }
+      }
+    });
+}
